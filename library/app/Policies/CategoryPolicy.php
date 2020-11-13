eefbe09d5852b5d1126ae96b549a3270e668bab4
ngoclap585 <ngoclap858@gmail.com>
2020-11-13 12:06:20 +0700

    push project up

---
+++ library/app/Policies/CategoryPolicy.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Policies;
+
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CategoryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function before(User $user)
+    {
+        if($user->level == 1)
+        {
+            return true;
+        }     
+    }
+    // public function anyview(User $user)
+    // {
+    //     //
+    // }
+
+    public function view(User $user)
+    {
+        return $user->level == 2;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->level == 2;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function update(User $user)
+    {
+        return $user->level == 2;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function delete(User $user)
+    {
+        return $user->level == 2;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function restore(User $user)
+    {
+        return $user->level == 2;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function forceDelete(User $user)
+    {
+        return $user->level == 2;
+    }
+}
